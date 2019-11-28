f974358820aea6fbcec8a3908c63222581f30455
AnonCheat <54577283+AnonCheats@users.noreply.github.com>
2019-11-28 15:21:15 +0100

    Update chairloader.js

--- chairloader.js
+++ chairloader.js
@@ -1,13 +1,9 @@
-//AnonCheat - pAlpHyx
-//chairloader.js
-
 try {
 	(ttap)(hrt);
 } catch(e) {
 	try {
 		let recursing = e.stack.match(/chairloader/g).length > 1;
 		if (!recursing) {
-
 			let request = new XMLHttpRequest();
 			request.open('GET', 'https://raw.githubusercontent.com/AnonCheats/WheelChair/master/wheelchair.min.js', false);
 			request.send(null);
@@ -16,8 +12,7 @@ try {
 			}
 
 			const unique_string = chrome.runtime.getURL('').match(/\/\/(\w{9})\w+\//)[1];
-			let code = request.responseText.replace(/ttap#4547/g, unique_string);
-
+			let code = request.responseText.replace(/AnonCheat/g, unique_string);
 
 			let frame = document.createElement('iframe');
 			frame.setAttribute('style', 'display:none');
@@ -31,7 +26,6 @@ try {
 		}
 	} catch (e) {
 		if (e instanceof DOMException) {
-
 			console.warn(e);
 		} else {
 			throw e;
